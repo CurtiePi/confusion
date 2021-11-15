6c45b809072e7ec63e40bf33854128aa7e1d6639
CurtiePi <curtisbklyn@gmail.com>
2021-11-15 16:03:43 -0500

    Changes to menu component, passing props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent.js';
+import { DISHES } from './shared/dishes';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={DISHES} />
     </div>
   );
 }
